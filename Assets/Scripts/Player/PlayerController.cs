792c2389b9087390b4e3e70d7aeb0c4a516f9d36
Giova <giovanni.aravena.13@sansano.usm.cl>
2019-09-24 02:53:12 -0300

    gitflow-feature-stash: PlayerController
    movimiento básico del puma:
    - saltar, correr, agazapado (agachado)
    - nuevo background al asset para MainMenu

---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public CharacterController2D controller;
+    public float runSpeed = 40f;
+
+    float horizontalMove = 0f;
+    bool jump = false;
+    bool crouch = false;
+
+    private Animator animator;
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+        //if (Input.GetKeyDown(KeyCode.UpArrow))
+        if (Input.GetButtonDown("Jump"))
+        {
+            jump = true;
+        }
+
+        //agacharse
+        if (Input.GetButtonDown("Crouch"))
+        {
+            crouch = true;
+        }else if (Input.GetButtonUp("Crouch"))
+        {
+            crouch = false;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        controller.Move(horizontalMove * Time.fixedDeltaTime, crouch, jump); //move, crouch, jump
+        if (horizontalMove==0) 
+            animator.SetTrigger("PumaWait_Anim");
+        else
+            animator.SetTrigger("PumaRun_Anim");
+        jump = false;
+    }
+
+   
+}
